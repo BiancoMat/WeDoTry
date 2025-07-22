91dc9af27537455f5c257056080e8a15e8e9b467
BiancoMat <cristiano.bianchini@hotmail.it>
2025-07-22 14:38:42 +0200

    Update App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,1 +1,16 @@
-// Entry point, will be filled later
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
