6e973ceacab7f853843a09f129ee7365a4e2faa6
BiancoMat <cristiano.bianchini@hotmail.it>
2025-07-24 21:08:23 +0000

    fix: moved index.html to root for Vercel

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,1 +1,14 @@
-// Entry point, will be filled later
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
